1404fc3d75a5c591ab5358dadbdea29883211a5c
Ahmed Mohsen <ahmed.mohsenmm8@gmail.com>
2025-07-08 19:41:17 +0300

    Add files via upload

---
+++ bike_sahre.sql
@@ -0,0 +1,19 @@
+WITH cte as ( 
+SELECT * FROM bike_share_yr_0
+UNION ALL 
+SELECT * FROM bike_share_yr_1
+)
+SELECT 
+dteday,
+season,
+a.yr,
+weekday,
+hr,
+rider_type,
+riders,
+price,
+COGS,
+riders* price as revenue,
+(riders* price) - (COGS * riders) as profit 
+FROM cte a
+LEFT JOIN cost_table b ON a.yr = b.yr
